04a17afec3ab2aefd20e209c81990a88bc1d2ca1
sachin yadav <ssachinyadav6537@gmail.com>
2026-09-10 14:10:32 +0530

    logout user

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -36,7 +36,7 @@ const userSchema = new mongoose.Schema({
 
 // Generate JWT Token
 userSchema.methods.generateAuthToken = function () { 
-    const token =jwt.sign({_id: this._id},process.env.JWT_SECRET)
+    const token =jwt.sign({_id: this._id},process.env.JWT_SECRET , {expiresIn:'24'})
     return token ;
      };
 
