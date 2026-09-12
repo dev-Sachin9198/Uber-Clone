4d4e52d06f5b312ec653ef96135e9b3f047a6dca
sachin yadav <ssachinyadav6537@gmail.com>
2026-09-12 15:19:53 +0530

    add UserSignup Captainlogin captainsignup

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,15 +1,34 @@
+import  { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const UserLogin = () => {
+   const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+  
+ const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(email,password)  
+     setEmail('')
+    setPassword('')
+    
+ }
+ 
+
   return (
     <div className='p-7 h-screen flex flex-col justify-between'>
       <div>
         <img className='w-16 mb-10' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYQy-OIkA6In0fTvVwZADPmFFibjmszu2A0g&s" alt="" />
 
-        <form>
+        <form  onSubmit={(e) => {
+          submitHandler(e)
+        }}>
           <h3 className='text-lg font-medium mb-2'>What's your email</h3>
           <input
             required
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value)
+            }}
             className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
             type="email"
             placeholder='email@example.com'
@@ -19,6 +38,10 @@ const UserLogin = () => {
 
           <input
             className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
             required type="password"
             placeholder='password'
           />
